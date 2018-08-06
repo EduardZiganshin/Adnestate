f331d8fdd94fa8e2678087fdfb2f5c1c9597e6f1
EduardZiganshin <zef1979@inbox.ru>
2018-08-06 12:49:53 +0400

    Переделал форму поиска

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,41 @@
-var expanded = false;
+// var expanded = false;
 
-function showCheckboxes() {
-    var checkboxesProperty = document.getElementById("checkboxesProperty");
-    if (!expanded) {
-        checkboxesProperty.style.display = "block";
-        expanded = true;
-    } else {
-        checkboxesProperty.style.display = "none";
-        expanded = false;
-    }
-}
+// function showCheckboxes() {
+//     var checkboxesProperty = document.getElementById("checkboxesProperty");
+//     if (!expanded) {
+//         checkboxesProperty.style.display = "block";
+//         expanded = true;
+//     } else {
+//         checkboxesProperty.style.display = "none";
+//         expanded = false;
+//     }
+// }
 
-var expanded1 = false;
+// var expanded1 = false;
 
-function showCheckboxes1() {
-    var checkboxesRoom = document.getElementById("checkboxesRoom");
-    if (!expanded1) {
-        checkboxesRoom.style.display = "block";
-        expanded1 = true;
-    } else {
-        checkboxesRoom.style.display = "none";
-        expanded1 = false;
-    }
-}
+// function showCheckboxes1() {
+//     var checkboxesRoom = document.getElementById("checkboxesRoom");
+//     if (!expanded1) {
+//         checkboxesRoom.style.display = "block";
+//         expanded1 = true;
+//     } else {
+//         checkboxesRoom.style.display = "none";
+//         expanded1 = false;
+//     }
+// }
 
-var expanded2 = false;
+// var expanded2 = false;
 
-function showCheckboxes2() {
-    var checkboxesRoom = document.getElementById("checkboxesAuction");
-    if (!expanded2) {
-        checkboxesAuction.style.display = "block";
-        expanded2 = true;
-    } else {
-        checkboxesAuction.style.display = "none";
-        expanded2 = false;
-    }
-}
+// function showCheckboxes2() {
+//     var checkboxesRoom = document.getElementById("checkboxesAuction");
+//     if (!expanded2) {
+//         checkboxesAuction.style.display = "block";
+//         expanded2 = true;
+//     } else {
+//         checkboxesAuction.style.display = "none";
+//         expanded2 = false;
+//     }
+// }
 
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
